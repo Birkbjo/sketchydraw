c4282f0b9e67c4036e0ab4804a88f985873ec333
birkbjo <birkbjo@gmail.com>
2016-06-27 02:51:56 +0200

    Add handling of cookie secret. Add secret file to gitignore aswell.

--- server/setup.js
+++ server/setup.js
@@ -13,15 +13,22 @@ var util = require('util');
 
 //Port to listen to
 var port = require('../public/resources/js/setup.json').port;
+var SESSION_SECRET;
+
+try { //try to load a secret from env or from json file in root dir
+    SESSION_SECRET = process.env.SESSION_SECRET || require('../secret.json').secret;
+} catch(e) { //fallback to a default value
+    SESSION_SECRET = 'very_secret_string';
+}
 
 var sessionMiddleware = session({
-    secret: 'abcd',
+    secret: SESSION_SECRET,
     resave: 'false',
     saveUninitialized: 'false',
     cookie: {maxAge: 24 * 60 * 60 * 1000 }
 });
 
-app.use(cookieParser());
+//app.use(cookieParser());
 app.use(bodyParser.urlencoded({
     extended: true
 }));
